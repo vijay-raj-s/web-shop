9a4e4560d0d3c7b2c452856a776cccc7b060f76b
Vijay Raj <vijay.rs2992@gmail.com>
2019-11-27 15:27:28 +0100

    Initial Commit with package structure

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+       App works!
+    </div>
+  );
+}
+
+export default App;
