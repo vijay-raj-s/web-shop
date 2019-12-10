0dfa715669b85bcd09e8f39832e6e408c0c04888
Vijay Raj <vijay.rs2992@gmail.com>
2019-12-10 15:52:29 +0100

    Added search layout

--- src/search/search-component.tsx
+++ src/search/search-component.tsx
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import './search.scss'; 
+import Grid from '@material-ui/core/Grid';
 
 export default class SearchComponent extends Component {
   render() {
     return (
-      <div className="search">
-        Search works!
+      <div className="search-container">
+        <Grid container>
+          <Grid item md={10}> 
+              <div className='search-listing'>
+                search
+              </div>
+          </Grid>
+          <Grid item md={2}> 
+              Item description
+          </Grid>
+        </Grid>
       </div>
     )
   }
