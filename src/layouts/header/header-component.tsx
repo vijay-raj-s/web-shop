aca03382334bd18bf70e3aeedf5b9348b3e2d646
mauricelux <maumau.chrisnach@gmail.com>
2019-12-18 11:01:28 +0100

    Dont really know why this changed

--- src/layouts/header/header-component.tsx
+++ src/layouts/header/header-component.tsx
@@ -4,7 +4,7 @@ import logo from '../../assets/images/LOGO.svg';
 import profile from '../../assets/images/profile.svg';
 import shoppingCart from '../../assets/images/shopping-cart.svg';
 import Button from '@material-ui/core/Button';
-import Badge from '@material-ui/core/Badge'; 
+import Badge from '@material-ui/core/Badge';
 import Axios from 'axios';
 
 
